a66f15db8fc3230fc04428333a06eb92c87b6ac9
Tiago Mota <tmota.seg@gmail.com>
2026-03-29 01:14:35 -0300

    feat: saudação com Console ao invés de Scanner.

---
+++ exercicios-conhecendo-a-base/src/console/SaudacaoConsole.java
@@ -0,0 +1,22 @@
+/* Saudação no console.
+ *
+ * Programa que lê um nome e imprime uma saudação, utilizando o
+ * Console ao invés do Scanner.
+ * 
+ * Javadoc: https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/io/Console.html
+ * 
+ */
+
+package console;
+
+import java.io.Console;
+
+public class SaudacaoConsole {
+    public static void main(String[] args) {
+        Console console = System.console();
+
+        System.out.println("Digite seu nome: ");
+        String nome = console.readLine();
+        System.out.printf("Olá, %s! Seja bem-vindo!%n", nome);
+    }
+}
