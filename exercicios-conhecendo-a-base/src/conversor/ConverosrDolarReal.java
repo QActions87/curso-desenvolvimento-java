aa8e33cfb0beb15867288a85d025da5e0c5c012f
Tiago Mota <tmota.seg@gmail.com>
2026-03-29 00:32:44 -0300

    feat: programa/Conversor de dólares em reais.

---
+++ exercicios-conhecendo-a-base/src/conversor/ConverosrDolarReal.java
@@ -0,0 +1,27 @@
+/* Conversor de Dólar para Real. 
+ *
+ * Programa que coverte um valor em dólar para real.
+ * 
+ * Ex.: Dólares = 100, Cotação = 4,92 => O valor em reais é R$: 492,00.
+ */
+package conversor;
+
+import java.util.Scanner;
+
+public class ConverosrDolarReal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite o valor em dólar:");
+        double dolares = scanner.nextDouble();
+        
+        System.out.println("Digite a cotação do dólar:");
+        double cotacao = scanner.nextDouble();
+        
+        double reais = dolares * cotacao;
+        
+        System.out.printf("O valor em reais é R$: %.2f%n", reais);
+
+        scanner.close();
+    }
+}
