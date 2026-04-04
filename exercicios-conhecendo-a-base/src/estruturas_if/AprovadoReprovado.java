d293216f39adef125d1ce5d4a3833495b1d8b3a3
Tiago Mota <tmota.seg@gmail.com>
2026-04-04 00:59:27 -0300

    feat: If-Else/Programa: checador de notas.

---
+++ exercicios-conhecendo-a-base/src/estruturas_if/AprovadoReprovado.java
@@ -0,0 +1,25 @@
+package estruturas_if;
+
+import java.util.Scanner;
+
+public class AprovadoReprovado {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Leitura das notas:
+        System.out.println("Digite a nota do aluno: ");
+        double nota1 = scanner.nextDouble();
+        double nota2 = scanner.nextDouble();
+        double nota3 = scanner.nextDouble();
+        // Cálculo da média:
+        double media = (nota1 + nota2 + nota3) / 3;
+        // Verificação da aprovação:
+        if (media >= 7.0) {
+            System.out.println("Aluno aprovado!");
+        } else {
+            System.out.println("Aluno reprovado!");
+        }
+        // Fechamento do recurso scanner:
+        scanner.close();
+
+    }
+}
