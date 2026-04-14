1ec89cf1309ae086666ae268e4482c4dcca7fba9
Tiago Mota <tmota.seg@gmail.com>
2026-04-14 17:32:09 -0300

    feat: Array/1º array: Declaração do array, inicialização e acesso aos seus elementos.

---
+++ arrays-java/arrays-java/src/ArrayStructure.java
@@ -0,0 +1,11 @@
+public class ArrayStructure {
+    public static void main(String[] args) {
+        // Declarar e Inicializar um array de Strings:
+        String[] frutas = {"Maçã", "Banana", "Laranja", "Uva"};
+        // Acessar elementos do array:
+        System.out.println(frutas[0]); // Imprime "Maçã"
+        System.out.println(frutas[1]); // Imprime "Banana"
+        System.out.println(frutas[2]); // Imprime "Laranja"
+        System.out.println(frutas[3]); // Imprime "Uva"
+    }
+}
