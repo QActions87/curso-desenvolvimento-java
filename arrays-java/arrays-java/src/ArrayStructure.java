b3b86cc38bdee0700fdddf59fd651954e0fb14fc
Tiago Mota <tmota.seg@gmail.com>
2026-04-21 16:27:34 -0300

    Strig[] Args: Passando argumentos pelo terminal, em tempo de execução.

--- arrays-java/arrays-java/src/ArrayStructure.java
+++ arrays-java/arrays-java/src/ArrayStructure.java
@@ -1,20 +1,25 @@
 public class ArrayStructure {
     public static void main(String[] args) {
         // Declarar e Inicializar um array de Strings:
-        String[] frutas = {"Maçã", "Banana", "Laranja", "Uva"};
+        String [] frutas = {"Maçã", "Banana", "Laranja", "Uva"};
         // Acessar elementos do array:
         System.out.println(frutas[0]); // Imprime "Maçã"
         System.out.println(frutas[1]); // Imprime "Banana"
         System.out.println(frutas[2]); // Imprime "Laranja"
         System.out.println(frutas[3]); // Imprime "Uva"
-        
+        // Imprimir os argumentos passados para o programa:
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+         /*
         // Modificar o 2º elemento do array (de:Banana para:Abacaxi):
         frutas[1] = "Abacaxi";
         System.out.println(frutas[1]); // Imprime "Abacaxi" 
         
         // Verificar tamanho do array:
         System.out.println("Tamanho do array: " + frutas.length); // Imprime "Tamanho do array: 4"
-
+         
         // Iterar sobre o array usando um loop for:
         for (int i = 0; i < frutas.length; i++) {
             System.out.println(frutas[i] + " está na posição " + i);
@@ -25,5 +30,8 @@ public class ArrayStructure {
             System.out.println("A fruta: " + fruta + " está na posição: " + i);
             i++; // Incrementa o contador para a próxima posição
         }
+        */
+        
+        
     }
 }
