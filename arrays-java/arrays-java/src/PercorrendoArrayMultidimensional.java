bf8377947c4765def0d1b8965a9b37cb7edb53a5
Tiago Mota <tmota.seg@gmail.com>
2026-05-09 20:55:46 -0300

    Array/Array Multidimensional: Percorrendo o Array Multidimensional com o loop Foreach aninhado.

--- arrays-java/arrays-java/src/PercorrendoArrayMultidimensional.java
+++ arrays-java/arrays-java/src/PercorrendoArrayMultidimensional.java
@@ -1,9 +1,14 @@
 public class PercorrendoArrayMultidimensional {
     public static void main(String[] args) {
         int multidimencional [] [] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        // 
-        for(int i)
-
+        
+        // Iterando sobre o array multidimensional usando loops Foreach aninhados:
+        for(int[] i : multidimencional) {
+            for(int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println(); // Imprime uma nova linha após cada linha do array
+        }
 
     }
 }
