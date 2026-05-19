1678e0d60db0bbe1837b73b811f6f22a5998efda
Tiago Mota <tmota.seg@gmail.com>
2026-05-19 16:41:48 -0300

    O.O./Métodos: Classe Casa com métodos e atributos.

---
+++ trabalhando-com-objetos/src/Casa.java
@@ -0,0 +1,12 @@
+public class Casa {
+    boolean inteligente;
+
+    void ligarLuzes() {
+      if (inteligente) {
+        System.out.println("comando de Voz: ligar luzes");
+      } else {
+        System.out.println("luzes ligadas manualmente");
+      }  
+        
+    }
+}
