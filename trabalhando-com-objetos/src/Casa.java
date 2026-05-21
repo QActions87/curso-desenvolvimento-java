d8e734c6de0e2093c8a8aec65519c7570c800817
Tiago Mota <tmota.seg@gmail.com>
2026-05-21 17:12:59 -0300

    POO/Construtores com argumentos.

--- trabalhando-com-objetos/src/Casa.java
+++ trabalhando-com-objetos/src/Casa.java
@@ -1,6 +1,10 @@
 public class Casa {
     boolean inteligente;
 
+    public Casa(boolean inteligente) {
+        this.inteligente = inteligente;
+    }
+
     void ligarLuzes() {
       if (inteligente) {
         System.out.println("comando de Voz: ligar luzes");
--- trabalhando-com-objetos/src/Main.java
+++ trabalhando-com-objetos/src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Casa casa1 = new Casa();
-        Casa casa2 = new Casa();
-
-        casa1.inteligente = false;
-        casa2.inteligente = true;
+        Casa casa1 = new Casa(false);
+        Casa casa2 = new Casa(true);
 
         casa1.ligarLuzes();
         casa2.ligarLuzes();
