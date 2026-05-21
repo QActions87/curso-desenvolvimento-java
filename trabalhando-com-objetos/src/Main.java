c0603721f43d62510c2669b314181e9638f9cc01
Tiago Mota <tmota.seg@gmail.com>
2026-05-21 16:35:14 -0300

    POO/Construtores padrão: Instanciando aa classe, criando objetos e chamando o método 'inteligente' neles.

--- trabalhando-com-objetos/src/Main.java
+++ trabalhando-com-objetos/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Casa casa1 = new Casa();
+        Casa casa2 = new Casa();
+
+        casa1.inteligente = false;
+        casa2.inteligente = true;
+
+        casa1.ligarLuzes();
+        casa2.ligarLuzes();
     }
 }
