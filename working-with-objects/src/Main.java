f0aeb6e7f6fc0385f8281f166fec3369cfd6c6f9
Tiago Mota <tmota.seg@gmail.com>
2026-06-09 02:02:18 -0300

    atualizando...

--- working-with-objects/src/Main.java
+++ working-with-objects/src/Main.java
@@ -1,14 +1,17 @@
+import moradias.Casa;
+
 public class Main {
     public static void main(String[] args) {
         Casa casa1 = new Casa(false);
         Casa casa2 = new Casa(true);
         Casa casa3 = new Casa();
 
-        casa3.inteligente = false;
-
         casa1.ligarLuzes();
         casa2.ligarLuzes();
         casa3.ligarLuzes();
+
+        casa3.nome = "Zion";
+        System.out.println(casa3.nome);
     }
 }
 
--- working-with-objects/src/moradias/Casa.java
+++ working-with-objects/src/moradias/Casa.java
@@ -1,16 +1,21 @@
-// Classe Casa:
+package moradias;
+
+// Classe moradias.Casa:
 public class Casa {
     boolean inteligente;
+    String nome;
+
     // Construtor Padrão, sem parâmetros:
     public Casa() {
 
     }
     // Construtor com parâmetros:
     public Casa(boolean inteligente) {
+
         this.inteligente = inteligente;
     }
 
-    // Método para ligar as luzes:
+    // Metodo para ligar as luzes:
     void ligarLuzes() {
       if (inteligente) {
         System.out.println("comando de Voz: ligar luzes");
