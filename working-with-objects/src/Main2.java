93e69d5c85477793caecd6c7d88309216be96819
Tiago Mota <tmota.seg@gmail.com>
2026-06-10 20:09:24 -0300

    Modificador Static/Atributo Static: Criação do atributo.

---
+++ working-with-objects/src/Main2.java
@@ -0,0 +1,8 @@
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        Double soma = Matematica.soma(5.0, 4.0);
+        System.out.println(soma);
+    }
+}
--- working-with-objects/src/Matematica.java
+++ working-with-objects/src/Matematica.java
@@ -1,5 +1,7 @@
 public class Matematica {
-    public Double soma (double n1, double n2) {
+    public Double limite = 100.0;
+
+    public static Double soma (double n1, double n2) {
         return n1 + n2;
     }
 }
