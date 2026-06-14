c477c70cabb5f7c7b2aa87e3cd59097f9b18c37c
Tiago Mota <tmota.seg@gmail.com>
2026-06-14 03:01:52 -0300

    Atualizando...

--- working-with-objects/src/Main2.java
+++ working-with-objects/src/Main2.java
@@ -3,6 +3,8 @@ public class Main2 {
     public static void main(String[] args) {
 
         Double soma = Matematica.soma(5.0, 4.0);
+        Double somaNaoEstatica = new Matematica().somaNaoEstatica(6.0, 7.0);
+        System.out.println(somaNaoEstatica);
         System.out.println(soma);
         System.out.println(Matematica.limite);
     }
--- working-with-objects/src/Matematica.java
+++ working-with-objects/src/Matematica.java
@@ -7,4 +7,11 @@ public class Matematica {
             return soma;
         else return soma;
     }
+
+    public Double somaNaoEstatica(double n1, double n2) {
+        double soma = n1 + n2;
+        if (soma > limite)
+            return soma;
+        else return soma;
+    }
 }
