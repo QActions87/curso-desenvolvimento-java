b163009b46284241d4e66bf0073ace42cce31121
Tiago Mota <tmota.seg@gmail.com>
2026-06-14 00:43:26 -0300

    Modificador Static/Atributo Static: tornando atributo static para acessá-lo a partir de um método static.

--- working-with-objects/src/Matematica.java
+++ working-with-objects/src/Matematica.java
@@ -1,7 +1,10 @@
 public class Matematica {
-    public Double limite = 100.0;
+    public static Double limite = 100.0;
 
     public static Double soma (double n1, double n2) {
-        return n1 + n2;
+        double soma = n1 + n2;
+        if (soma > limite)
+            return soma;
+        else return soma;
     }
 }
