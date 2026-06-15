434d89fd51d9f51b75a7fe60257015458f99ebce
Tiago Mota <tmota.seg@gmail.com>
2026-06-15 12:57:46 -0300

    Objeto Array/ Criando um array de inteiros e acessando 1ª e 2ª posições.

---
+++ working-with-objects/src/ArrayNotasEstudos.java
@@ -0,0 +1,11 @@
+
+public class ArrayNotasEstudos {
+
+    public static void main(String[] args) {
+        int[] idades = {18, 20, 30};
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+    }
+}
+
