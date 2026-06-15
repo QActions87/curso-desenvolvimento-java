c598f05a538f249d5ceb5b7e900cafb3991b00c7
Tiago Mota <tmota.seg@gmail.com>
2026-06-15 11:49:24 -0300

    Modificadores/ final: implementando a constante 'LIMITE_OPERACIONAL' em SCREAMING_SNAKE_CASE, pois se tornou uma constante através do modificador 'final'.

--- working-with-objects/src/Main2.java
+++ working-with-objects/src/Main2.java
@@ -6,6 +6,6 @@ public class Main2 {
         Double somaNaoEstatica = new Matematica().somaNaoEstatica(6.0, 7.0);
         System.out.println(somaNaoEstatica);
         System.out.println(soma);
-        System.out.println(Matematica.limite);
+        System.out.println(Matematica.LIMITE_OPERACIONAL);
     }
 }
--- working-with-objects/src/Matematica.java
+++ working-with-objects/src/Matematica.java
@@ -1,17 +1,22 @@
 public class Matematica {
-    public static Double limite = 100.0;
+    public static final Double LIMITE_OPERACIONAL = 100.0;
 
     public static Double soma (double n1, double n2) {
         double soma = n1 + n2;
-        if (soma > limite)
+        if (soma > LIMITE_OPERACIONAL) {
+            return LIMITE_OPERACIONAL;
+        } else {
             return soma;
-        else return soma;
+        }
     }
 
     public Double somaNaoEstatica(double n1, double n2) {
         double soma = n1 + n2;
-        if (soma > limite)
+
+        if (soma > LIMITE_OPERACIONAL) {
+            return LIMITE_OPERACIONAL;
+        } else {
             return soma;
-        else return soma;
+        }
     }
 }
