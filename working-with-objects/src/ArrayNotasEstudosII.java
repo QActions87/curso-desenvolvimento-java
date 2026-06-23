55a957d3b26ec8047f09dc29e66e3df1ffe26fa2
Tiago Mota <tmota.seg@gmail.com>
2026-06-23 02:49:58 -0300

    Arrays/Arrays de Objetos: Criação do array gatos do tipo Gato com duas instâncias da classe Gato como parâmetros e uso do método 'toString' da classe utilitária 'Arrays' para converter o array gatos em string. Porém, os elementos são objetos e ainda retornarão Hash codes.

---
+++ working-with-objects/src/ArrayNotasEstudosII.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class ArrayNotasEstudosII {
+
+    public static void main(String[] args) {
+
+        Gato[] gatos = {new Gato("hórus"), new Gato("Sophia")};
+
+        System.out.println(Arrays.toString(gatos));
+    }
+}
