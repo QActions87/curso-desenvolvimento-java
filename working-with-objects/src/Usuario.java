8d80c3686125832b149e719f5e749ec7dbc31040
Tiago Mota <tmota.seg@gmail.com>
2026-06-25 02:45:35 -0300

    Arrays/Arrays de Objetos: finalizando comparação entre objetos.

--- working-with-objects/src/Usuario.java
+++ working-with-objects/src/Usuario.java
@@ -1,5 +1,26 @@
+import java.util.Objects;
+
 public class Usuario {
     public String nome;
     public String email;
     public String senha;
+
+    // Representação textual dos atributos:
+    public String toString() {
+        return "{ Nome: " + nome + ", Email: " + email + ", Senha: " + senha +" }";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(nome, usuario.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nome);
+    }
 }
+
+
