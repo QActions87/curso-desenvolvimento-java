5f8ff352e608e601b942ec4df7b5370d00b439e8
Tiago Mota <tmota.seg@gmail.com>
2026-07-06 17:11:04 -0300

    POO/Encapsulamento: Criando os métodos Getters e Setters na classe Carro.

--- principios-poo/src/Carro.java
+++ principios-poo/src/Carro.java
@@ -1,11 +1,11 @@
 public class Carro {
-
+    // Atributos privados:
     private String volante;
     private String pedais;
-
     private Motor motor;
     private String airbag;
 
+    // Métodos:
     public void ligarCarro() {
         System.out.println("Ligando o carro...");
     }
@@ -25,5 +25,14 @@ public class Carro {
 
     private void ativarAirbags() {
         System.out.println("Ativando airbags...");
+    }
+
+    // Métodos Getters e Setters:
+    public String getPedais() {
+        return pedais;
+    }
+
+    public void setPedais(String pedais) {
+        this.pedais = pedais;
     }
 }
