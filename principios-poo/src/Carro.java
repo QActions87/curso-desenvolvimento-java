8bd7bd1d8a8050d16015bb8811b22671ba5c07c2
Tiago Mota <tmota.seg@gmail.com>
2026-07-06 17:16:23 -0300

    POO/Encapsulamento: Criando os demais métodos Getters e Setters...

--- principios-poo/src/Carro.java
+++ principios-poo/src/Carro.java
@@ -8,13 +8,13 @@ public class Carro {
     // Métodos:
     public void ligarCarro() {
         System.out.println("Ligando o carro...");
-    }
 
+    }
     public void acelerar() {
         motor.iniciar();
         System.out.println("Acelerando...");
-    }
 
+    }
     public void frear() {
         System.out.println("Freando....");
     }
@@ -34,5 +34,29 @@ public class Carro {
 
     public void setPedais(String pedais) {
         this.pedais = pedais;
+    }
+
+    public String getVolante() {
+        return volante;
+    }
+
+    public void setVolante(String volante) {
+        this.volante = volante;
+    }
+
+    public String getAirbag() {
+        return airbag;
+    }
+
+    public void setAirbag(String airbag) {
+        this.airbag = airbag;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
     }
 }
