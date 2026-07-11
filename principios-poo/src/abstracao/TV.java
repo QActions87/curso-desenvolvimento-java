e0d7566a8b289064a84135108f6e687e0e742106
Tiago Mota <tmota.seg@gmail.com>
2026-07-10 23:43:59 -0300

    Abstração: Criando o método ligarDesligar().

--- principios-poo/src/abstracao/TV.java
+++ principios-poo/src/abstracao/TV.java
@@ -1,7 +1,18 @@
 package abstracao;
 
 public class TV {
+    // Atributos encapsulados:
     private boolean ligada;
     private int volume;
     private int canal;
+
+    // Métodos:
+    public void ligarDesligar() {
+        this.ligada = !this.ligada;
+        System.out.println(
+          "A TV está " + (ligada ? "ligada" : "desligada")
+        );
+    }
+
+
 }
