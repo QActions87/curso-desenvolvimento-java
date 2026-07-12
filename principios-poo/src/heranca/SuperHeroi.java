29e0615927cf5f858de230169c9a0828bba726dc
Tiago Mota <tmota.seg@gmail.com>
2026-07-12 00:44:37 -0300

    feat: Herança: Criando o construtor canônico que inicializa todos os atritutos da classe SuperHeroi.

--- principios-poo/src/heranca/SuperHeroi.java
+++ principios-poo/src/heranca/SuperHeroi.java
@@ -1,5 +1,27 @@
 package heranca;
 
 public class SuperHeroi {
+    // Atributos:
+    private String traje;
+    private String[] superPoderes;
 
+    // Construtor canônico (que inicializa todos os atributos da classe):
+    public SuperHeroi(String traje, String[] superPoderes) {
+        this.traje = traje;
+        this.superPoderes = superPoderes;
+    }
+
+    public SuperHeroi(String traje) {
+        this.traje = traje;
+
+    }
+
+    public SuperHeroi() {
+
+    }
+
+    // Métodos:
+    public void usarSuperPoder(int index) {
+        System.out.println("Usando super poder " + superPoderes[index]);
+    }
 }
