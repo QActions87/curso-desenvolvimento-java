d4a60a3f31766c8b09b87de11df7671c29d36450
Tiago Mota <tmota.seg@gmail.com>
2026-07-11 23:53:17 -0300

    feat: Herança: Criando classe main para instanciar as classes.

---
+++ principios-poo/src/heranca/Main.java
@@ -0,0 +1,10 @@
+package heranca;
+
+public class Main {
+    public static void main(String[] args) {
+        HomemAranha homemAranha = new HomemAranha();
+        HomemDeFerro homemDeFerro = new HomemDeFerro();
+        Tempestade tempestade = new Tempestade();
+        ViuvaNegra viuvaNegra = new ViuvaNegra();
+    }
+}
